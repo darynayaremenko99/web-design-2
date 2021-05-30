bba0a019242c2879a53f9fed773bf4d386e2dbff
darynayaremenko99 <70691306+darynayaremenko99@users.noreply.github.com>
2021-05-30 16:21:57 +0300

    web3

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,57 +1,65 @@
-import pollListView from "../view/pollListView.js";
-import poll from "../model/poll.js";
-import pollList from "../model/pollList.js"
-import pollView from "../view/pollView.js";
-import answer from "../model/answer.js";
-
 export default class Controller{
-    constructor(pollListFromView){
-        console.log("controller");
-        this.pollListView = new pollListView();
-        this.pollList = new pollList();        
-        console.log(pollListFromView);
-        if(pollListFromView === null || pollListFromView === undefined){
-            this.pollList = new pollList();
-        } else{
-            this.pollList = pollListFromView;
-        }
-        this.createPollButton = document.querySelector("#createPollButton");
-        this.passPollButton = document.querySelector("#passPollButton");
-        this.mainBlock = document.querySelector("#main");
-        this.createPollButton.addEventListener("click", this.onCreate.bind(this));
-        this.passPollButton.addEventListener("click", this.onPass.bind(this));
-        
-        
+    constructor(){
+        this.main = new Vue({ 
+            el: '#main',
+            data: {
+                create: false,
+                choose: true,
+                answer: false,
+                list: false,
+                saved: false,
+                poll:{
+                    question: "",
+                    answer1:"",
+                    answer2:"",
+                    answer3:""
+                },
+                pollList:[],
+                createAction: ()=>{
+                    this.main.create = true;
+                    this.main.choose = false;
+                },
+                passAction: ()=>{
+                    this.main.list = true;
+                    this.main.choose = false;
+                },
+                saveCreateAction: ()=>{
+                    this.main.pollList.push({...this.main.poll});
+                    this.main.poll = {
+                        question: "",
+                        answer1:"",
+                        answer2:"",
+                        answer3:""
+                    };  
+                    console.log(this.main.pollList);
+                    console.log(this.main.poll);
+                    this.main.create = false;
+                    this.showSaveMessage();
+                    axios.post('https://localhost:44351/api/poll/', this.main.poll);
+                },
+                passPollQuestion: (question)=>{
+                    this.main.poll = this.main.pollList.find(poll => poll.question == question);
+                    this.main.list = false;
+                    this.main.answer = true;
+                    axios.get('https://localhost:44351/api/pollList/')
+                    .then((response) => {
+                        this.main.pollList = response;
+                    });
+                },
+                savePassAction: ()=>{
+                    this.main.answer = false;
+                    this.main.create = false;
+                    this.showSaveMessage();
+                }
+            }
+        });   
     }
 
-    onCreate(){
-        console.log("on create");
-        this.pollView = new pollView(this.pollList);
-        this.pollView.createPoll(); 
-
-        //this.generateRandomPoll();
-      
+    showSaveMessage(){
+        this.main.saved = true;
+        setTimeout(()=>{
+            this.main.saved = false;
+            this.main.choose = true;
+        },2000);
     }
-    generateRandomPoll(){
-        let answer1 = new answer();
-        let answer2 = new answer();
-        answer1.text = "text of answer 1";
-        answer2.text = "text of answer 2";
-        let poll1 = new poll();
-        poll1.question = "How are you doing?";
-        poll1.addAnswer(answer1);
-        poll1.addAnswer(answer2);
-        console.log(poll1);
-        this.pollList.addPoll(poll1);
-    }
-
-    onPass(){
-        console.log("on pass");
-        console.log(this.pollList);
-        this.pollListView = new pollListView(this.pollList.pollList); 
-        this.pollListView.renderPollList();
-    }
-
-
-
 }
--- js/model/answer.js
+++
@@ -1,7 +0,0 @@
-export default class answer{
-    constructor(){
-        this.text = "";
-    }
-
-
-}
